39a4f4be66f22d01c7b09f1e79a8160e65870782
sarnobat <ss401533@gmail.com>
2013-12-15 16:56:38 -0800

    Initial commit

--- webserver_json.groovy
+++ webserver_json.groovy
@@ -1,11 +1,14 @@
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collection;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.io.FileUtils;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.json.JSONException;
@@ -25,7 +28,14 @@ public class Server {
                 public Response json() throws JSONException {
                         JSONObject json = new JSONObject();
                         json.put("img", _dir + "/wp_Janubaba_DeepikaPadukonebycoolman_201032123814NINCA6.jpg");
-						json.put("foo", "bar");
+                        json.put("img2", _dir + "/wp_Janubaba_RaimaSen_200542323920O4R141-2335724f84eae731c503f88521bebab6.jpg");
+                        String[] extensions = { "jpg" };
+                        Collection<File> files = FileUtils.listFiles(new File(_dir), extensions,true);
+                        for (Object o : files) {
+                        	File f = (File)o;
+                        	json.put(f.getAbsolutePath(),f.getAbsolutePath());
+                        }
+                        
 					    return Response.ok().header("Access-Control-Allow-Origin", "*")
 						 			.entity(json.toString()).type("application/json").build();
                 }
